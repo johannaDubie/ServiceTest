d2505aca562d5e1f209083dd695964e745ef8257
johannaDubie <44159752+johannaDubie@users.noreply.github.com>
2019-02-05 14:55:03 +0100

    Add files via upload

---
+++ ServiceTest/ServiceTest/AccessBdd.cs
@@ -0,0 +1,98 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceTest
+{
+    class AccessBdd
+    {
+        /// <summary>
+        /// propriétés nécessaires à la classe
+        /// </summary>
+        private bool finCurseur = true; // fin du curseur atteinte
+        private MySqlConnection connection; // chaine de connexion
+        private MySqlCommand command; // commande d'envoi de la requête à la base de données
+        private MySqlDataReader reader; // commande de gestion du curseur
+
+        /// <summary>
+        /// Constructeur
+        /// </summary>
+        public AccessBdd()
+        {
+            String chaineConnection = "server=localhost; database=gsb_frais; user id=userGsb; pwd=secret";
+            this.connection = new MySqlConnection(chaineConnection); //Création de l'objet connection
+        }
+
+
+        /// <summary>
+        /// Fonction qui permet l'exécution d'une requête SELECT
+        /// </summary>
+        /// <param name="chaineRequete">requête à éxécuter</param>
+        public void reqSelect(string chaineRequete)
+        {
+            this.connection.Open(); //Ouverture de la connection
+            this.command = new MySqlCommand(chaineRequete, this.connection); //création de l'objet command
+            this.reader = this.command.ExecuteReader(); // appel du curseur
+            this.finCurseur = false; //pour que le curseur tourne, et ne s'arrête pas tout de suite : éxécution de la requête
+            this.suivant(); //Passage à la ligne suivante
+            this.connection.Close();
+        }
+
+
+
+        /// <summary>
+        /// Fonction qui permet l'exécution d'une requête UPDATE
+        /// </summary>
+        /// <param name="chaineRequete">requête à exécuter</param>        
+        public void reqUpdate(string chaineRequete)
+        {
+            this.connection.Open(); //Ouverture de la connection
+            this.command = new MySqlCommand(chaineRequete, this.connection); //création de l'objet command
+            this.command.ExecuteNonQuery(); //exécution de la requête  
+            this.finCurseur = true; //fermeture du curseur
+            this.connection.Close();
+        }
+
+
+        /// <summary>
+        /// Fonction qui permet de récupérer un champ
+        /// </summary>
+        /// <param name="nomChamp">nom du champ à récupérer</param>
+        /// <returns>retourne le contenu du champ passé en paramètre</returns>
+        public Object champ(string nomChamp)
+        {
+            return this.reader[nomChamp];
+        }
+
+        /// <summary>
+        /// Fonction qui permet de passer à la ligne suivante du curseur
+        /// </summary>
+        public void suivant()
+        {
+            if (!this.finCurseur)
+            {
+                this.finCurseur = !this.reader.Read();
+            }
+        }
+
+        /// <summary>
+        /// Fonction qui test la fin du curseur
+        /// </summary>
+        /// <returns>vrai pour "fin du curseur"</returns>
+        public Boolean fin()
+        {
+            return this.finCurseur;
+        }
+
+        /// <summary>
+        /// Méthode qui permet de fermer la connexion
+        /// </summary>
+        public void close()
+        {
+            this.connection.Close();
+        }
+    }
+}
---
+++ ServiceTest/ServiceTest/GestionDates.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceTest
+{
+    public abstract class GestionDates
+    {
+        /// <summary>
+        /// Fonction qui permet d'optimser le code
+        /// Elle fait une opération sur la date passée en paramètre
+        /// et la transcrypte en chaine de caractères
+        /// </summary>
+        /// <param name="date">une date au format DateTime</param>
+        /// <param name="opAFaire">valeur du calcul à faire</param>
+        /// <returns>mois : le mois sous la forme de deux chiffres</returns>
+        public static String optimisation(DateTime date, int opAFaire)
+        {
+            String mois = date.AddMonths(opAFaire).ToString("MM");
+            return mois;
+        }
+
+        /// <summary>
+        /// Méthode qui retourne le numéro du mois précedent
+        /// </summary>
+        /// <returns>Retourne le numéro du mois précédent de celui passé en paramètre
+        /// dans la surcharge de la méthode appellée</returns>
+        public static String getMoisPrecedent()
+        {
+            //Appel à la surcharge de la méthode getMoisPrecedent(), qui cette fois, attend une date en paramètre
+            return getMoisPrecedent(DateTime.Today);
+        }
+
+
+        /// <summary>
+        /// Surcharge de la méthode getMoisPrecedent()
+        /// Méthode qui retourne le numéro du mois précedent sous la forme de deux chiffres
+        /// </summary>
+        /// <param name="date">date passée en paramètre sous la forme DateTime</param>
+        /// <returns>mois : le mois sous la forme de 2 chiffres</returns>
+        public static String getMoisPrecedent(DateTime date)
+        {
+            String mois = optimisation(DateTime.Today, -1); //Récupération du mois
+            return mois;
+        }
+
+        /// <summary>
+        /// Méthode qui retourne le num du mois suivant
+        /// </summary>
+        /// <returns>Retourne le numéro du mois précédent de celui passé en paramètre
+        /// dans la surcharge de la méthode appellée</returns>
+        public static String getMoisSuivant()
+        {
+            return getMoisSuivant(DateTime.Today);
+        }
+
+        /// <summary>
+        /// Surcharge de la méthode getMoisSuivant()
+        /// Méthode qui retourne le numéro du mois suivantsous la forme de deux chiffres
+        /// </summary>
+        /// <param name="date">date passée en paramètre sous la forme DateTime</param>
+        /// <returns>mois : le mois sous la forme de deux chiffres</returns>
+        public static String getMoisSuivant(DateTime date)
+        {
+            String mois = optimisation(DateTime.Today, 1);
+            return mois;
+        }
+
+        /// <summary>
+        /// Méthode qui permet de retourner l'année qui correspond au mois précedent 
+        /// </summary>
+        /// <param name="date">date passée en paramètre sous la forme DateTime</param>
+        /// <returns>annee : l'année qui correspond au mois précédent celui passé en paramètre </returns>
+        public static String getAnnee(DateTime date)
+        {
+            date = date.AddMonths(-1);
+            String annee = date.ToString("yyyy");
+            return annee;
+        }
+
+        /// <summary>
+        /// Méthode qui vérifie si le jour passé en paramètre est entre deux jours précis
+        /// </summary>
+        /// <param name="jour1">première borne de l'interval à vérifier</param>
+        /// <param name="jour2">deuxième borne de l'interval à vérifier</param>
+        /// <returns>vrai si la date du jour est entre les deux bornes, sinon faux</returns>
+        public static Boolean entre(int jour1, int jour2)
+        {
+            return entre(jour1, jour2, DateTime.Today);
+        }
+
+        /// <summary>
+        /// Surcharge de la méthode entre() avec une date passée en plus en paramètre
+        /// </summary>
+        /// <param name="jour1">première borne de l'interval à vérifier</param>
+        /// <param name="jour2">deuxième borne de l'interval à vérifier</param>
+        /// <param name="date">date à vérifier, au format DateTime</param>
+        /// <returns>vrai si la date du jour est entre les deux bornes, sinon faux</returns>
+        public static Boolean entre(int jour1, int jour2, DateTime date)
+        {
+            int jour = date.Day; //Récupération du numéro d'aujourd'hui 
+            if (jour >= jour1 && jour <= jour2)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+    }
+}
